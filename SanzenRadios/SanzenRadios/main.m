400dd67113727468e2a47807677baaba6c773e01
Sanzensekai <nathael.beckler@uha.fr>
2013-11-25 16:29:27 +0100

    Initialization

---
+++ SanzenRadios/SanzenRadios/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SanzenRadios
+//
+//  Created by Cours iPhone on 25/11/13.
+//  Copyright (c) 2013 Cours iPhone. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
